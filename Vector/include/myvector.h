ce5cd8f46adda54fcf1f2f160aa78fe9fd247f3c
deviner <Ilarin75@yandex.ru>
2020-08-06 18:34:56 -0700

    Updated with a set of requirements for containers. such as: iterators initializer_list member access (at, [], front, back)"

--- Vector/include/myvector.h
+++ Vector/include/myvector.h
@@ -1,53 +1,86 @@
-// An std::vector like container for a learning processes
-// base source: https://lokiastari.com/blog/2016/03/12/vector-resize/index.html
+// An std::vector like container.
+// Mainly done for learning processes of memory mangment and various semantics.
+// Base source: https://lokiastari.com/blog/2016/03/12/vector-resize/index.html
 
-#include <cstddef>
-#include <iostream>
-#include <cstddef>
+
+#include <type_traits>
+#include <memory>
 #include <algorithm>
+#include <stdexcept>
+#include <iterator>
 
-template <typename T>
-class MyVector 
+template<typename T>
+class MyVector
 {
-	std::size_t m_capacity;
-	std::size_t m_length;
+public:
+	using value_type = T;
+	using reference = T&;
+	using const_reference = T const&;
+	using pointer = T*;
+	using const_pointer = T const*;
+	using iterator = T*;
+	using const_iterator = T const*;
+	using riterator = std::reverse_iterator<iterator>;
+	using const_riterator = std::reverse_iterator<const_iterator>;
+	using difference_type = std::ptrdiff_t;
+	using size_type = std::size_t;
+
+private:
+	size_type       m_capacity;
+	size_type       m_length;
 	T* m_buffer;
+
+	struct Deleter
+	{
+		void operator()(T* m_buffer) const
+		{
+			::operator delete(m_buffer);
+		}
+	};
 
 public:
 	MyVector(int capacity = 10)
 		: m_capacity(capacity)
 		, m_length(0)
-		, m_buffer(static_cast<T*>(::operator new(sizeof(T) * capacity)))
+		, m_buffer(static_cast<T*>(::operator new(sizeof(T)* capacity)))
+	{}
+	template<typename I>
+	MyVector(I begin, I end)
+		: m_capacity(std::distance(begin, end))
+		, m_length(0)
+		, m_buffer(static_cast<T*>(::operator new(sizeof(T)* m_capacity)))
 	{
-		std::cout << "! constructed !" << std::endl;
+		for (auto loop = begin; loop != end; ++loop) {
+			pushBackInternal(*loop);
+		}
 	}
+	MyVector(std::initializer_list<T> const& list)
+		: MyVector(std::begin(list), std::end(list))
+	{}
 	~MyVector()
 	{
-		for (int loop = 0; loop < m_length; ++loop) 
-		{
-			m_buffer[m_length - 1 - loop].~T();
-		}
-		::operator delete(m_buffer);
-		std::cout << "! destructed !" << std::endl;
+		std::unique_ptr<T, Deleter>     deleter(m_buffer, Deleter());
+		clearElements();
 	}
 	MyVector(MyVector const& copy)
 		: m_capacity(copy.m_length)
 		, m_length(0)
-		, m_buffer(static_cast<T*>(::operator new(sizeof(T) * m_capacity)))
+		, m_buffer(static_cast<T*>(::operator new(sizeof(T)* m_capacity)))
 	{
 		try {
-			for (int loop = 0; loop < copy.m_length; ++loop) 
-			{
+			for (int loop = 0; loop < copy.m_length; ++loop) {
 				push_back(copy.m_buffer[loop]);
 			}
 		} catch (...) {
-			for (int loop = 0; loop < m_length; ++loop) {
-				m_buffer[m_length - 1 - loop].~T();
-			}
-			::operator delete(m_buffer);
+			std::unique_ptr<T, Deleter>     deleter(m_buffer, Deleter());
+			clearElements();
 			throw;
 		}
-		std::cout << "! constructed !" << std::endl;
+	}
+	MyVector& operator=(MyVector const& copy)
+	{
+		copyAssign(copy);
+		return *this;
 	}
 	MyVector(MyVector&& move) noexcept
 		: m_capacity(0)
@@ -56,21 +89,11 @@ public:
 	{
 		move.swap(*this);
 	}
-	MyVector& operator=(MyVector const& copy)
-	{
-		// Copy and Swap idiom
-		MyVector<T> tmp(copy);
-		tmp.swap(*this);
-		return *this;
-	}
 	MyVector& operator=(MyVector&& move) noexcept
 	{
 		move.swap(*this);
 		return *this;
 	}
-
-	// Modifiers
-	// No-throw guarantee swap
 	void swap(MyVector& other) noexcept
 	{
 		using std::swap;
@@ -78,95 +101,133 @@ public:
 		swap(m_length, other.m_length);
 		swap(m_buffer, other.m_buffer);
 	}
-	void push_back(T const& value)
+
+	// Non-Mutating functions
+	size_type           size() const { return m_length; }
+	size_type           capacity() const { return m_capacity; }
+	bool                empty() const { return m_length == 0; }
+
+	// Validated element access
+	reference           at(size_type index) { validateIndex(index); return m_buffer[index]; }
+	const_reference     at(size_type index) const { validateIndex(index); return m_buffer[index]; }
+
+	// Non-Validated element access
+	reference           operator[](size_type index) { return m_buffer[index]; }
+	const_reference     operator[](size_type index) const { return m_buffer[index]; }
+	reference           front() { return m_buffer[0]; }
+	const_reference     front() const { return m_buffer[0]; }
+	reference           back() { return m_buffer[m_length - 1]; }
+	const_reference     back() const { return m_buffer[m_length - 1]; }
+
+	// Iterators
+	iterator            begin() { return m_buffer; }
+	riterator           rbegin() { return riterator(end()); }
+	const_iterator      begin() const { return m_buffer; }
+	const_riterator     rbegin() const { return const_riterator(end()); }
+
+	iterator            end() { return m_buffer + m_length; }
+	riterator           rend() { return riterator(begin()); }
+	const_iterator      end() const { return m_buffer + m_length; }
+	const_riterator     rend() const { return const_riterator(begin()); }
+
+	const_iterator      cbegin() const { return begin(); }
+	const_riterator     crbegin() const { return rbegin(); }
+	const_iterator      cend() const { return end(); }
+	const_riterator     crend() const { return rend(); }
+
+	// Comparison
+	bool operator!=(MyVector const& rhs) const { return !(*this == rhs); }
+	bool operator==(MyVector const& rhs) const
+	{
+		return  (size() == rhs.size())
+			&& std::equal(begin(), end(), rhs.begin());
+	}
+
+	// Mutating functions
+	void push_back(value_type const& value)
 	{
 		resizeIfRequire();
 		pushBackInternal(value);
+	}
+	void push_back(value_type&& value)
+	{
+		resizeIfRequire();
+		moveBackInternal(std::move(value));
+	}
+	template<typename... Args>
+	void emplace_back(Args&&... args)
+	{
+		resizeIfRequire();
+		emplaceBackInternal(std::move(args)...);
 	}
 	void pop_back()
 	{
 		--m_length;
 		m_buffer[m_length].~T();
 	}
-	void clear() noexcept
+	void reserve(size_type capacityUpperBound)
 	{
-		for (int loop = 0; loop < m_length; ++loop)
-		{
-			m_buffer[m_length - 1 - loop].~T();
-		}
-		m_length = 0;
-	}
-	void resize(std::size_t new_size)
-	{
-		if (new_size > m_length)
-		{
-			try
-			{
-				for (int loop = m_length; loop < new_size; ++loop)
-				{
-					T tmp = {};
-					push_back(tmp);
-				}
-			}
-			catch (...)
-			{
-				for (int loop = 0; loop < m_length; ++loop)
-				{
-					m_buffer[m_length - 1 - loop].~T();
-				}
-				::operator delete(m_buffer);
-				throw;
-			}
-		}
-		else if (new_size < m_length)
-		{
-			for (int loop = m_length; loop > new_size; --loop)
-			{
-				m_buffer[m_length - 1 - loop].~T();
-				m_length--;
-			}
-		}
-	}
-	//void erase(const size_t& pos)
-	
-	// Capacity
-	std::size_t capacity() const noexcept
-	{
-		return m_capacity;
-	}
-	std::size_t size() const { return m_length; }
-	void reserve(std::size_t capacityUpperBound)
-	{
-		if (capacityUpperBound > m_capacity)
+		if (capacityUpperBound > m_capacity) {
 			reserveCapacity(capacityUpperBound);
+		}
 	}
-
-	// Element access
-	T& operator[](std::size_t index) { return m_buffer[index]; }
-	T const& operator[](std::size_t index) const { return m_buffer[index]; }
-
 private:
-
-	// Memory allocation area
+	void validateIndex(size_type index) const
+	{
+		if (index >= m_length) {
+			throw std::out_of_range("Out of Range");
+		}
+	}
 	void resizeIfRequire()
 	{
-		if (m_length == m_capacity) 
-		{
-			std::cout << "resize" << std::endl;
-			std::size_t newCapacity = std::max(2.0, m_capacity * 1.5);
+		if (m_length == m_capacity) {
+			size_type newCapacity = std::max(2.0, m_capacity * 1.5);
 			reserveCapacity(newCapacity);
 		}
 	}
+	void reserveCapacity(size_type newCapacity)
+	{
+		MyVector<T> tmpBuffer(newCapacity);
+		simpleCopy(tmpBuffer);
+		tmpBuffer.swap(*this);
+	}
+
+	// Adds new element to the end using placement new
 	void pushBackInternal(T const& value)
 	{
 		new (m_buffer + m_length) T(value);
 		++m_length;
 	}
-	void reserveCapacity(std::size_t newCapacity)
+	// Move new element to the end using placement new
+	void moveBackInternal(T&& value)
 	{
-		MyVector<T> tmpBuffer(newCapacity);
-		std::for_each(m_buffer, m_buffer + m_length, [&tmpBuffer](T const& v) { tmpBuffer.pushBackInternal(v); });
-
-		tmpBuffer.swap(*this);
+		new (m_buffer + m_length) T(std::move(value));
+		++m_length;
+	}
+	// Move a list of elements to the end using placement new
+	template<typename... Args>
+	void emplaceBackInternal(Args&&... args)
+	{
+		new (m_buffer + m_length) T(std::move(args)...);
+		++m_length;
+	}
+	void simpleCopy(MyVector<T>& dst)
+	{
+		std::for_each(m_buffer, m_buffer + m_length,
+					  [&dst](T const& v) {dst.pushBackInternal(v); }
+		);
+	}
+	// Calls the destructor on all the members in reverse order
+	void clearElements()
+	{
+		for (int loop = 0; loop < m_length; ++loop) {
+			m_buffer[m_length - 1 - loop].~T();
+		}
+	}
+	// Copy and Swap idiom
+	void copyAssign(MyVector const& copy)
+	{
+		MyVector<T>  tmp(copy);
+		tmp.swap(*this);
 	}
 };
--- Vector/src/main.cpp
+++ Vector/src/main.cpp
@@ -2,6 +2,7 @@
 #define _CRTDBG_MAP_ALLOC
 
 #include "myvector.h"
+#include <iostream>
 
 #include <stdlib.h>
 #include <crtdbg.h>
@@ -9,78 +10,65 @@
 template <class T>
 void Print(const T& arg)
 {
-    std::cout << "The vector elements are : \n";
+	std::cout << "The vector elements are : \n";
 
-    for (std::size_t i = 0; i < arg.size(); i++)
-    {
-        std::cout << arg[i] << ' ';
-    }
-    std::cout << "\n";
+	for (std::size_t i = 0; i < arg.size(); i++) {
+		std::cout << arg[i] << ' ';
+	}
+	std::cout << "\n";
 }
 
 int main()
 {
-    {
-        MyVector<float> v;
+	{
+		MyVector<float> v;
 
-        std::cout << "v.push_back()" << std::endl;
-        int in = 1;
-        while (in > 0)
-        {
-            std::cin >> in;
-            v.push_back((float)in);
-        }
+		std::cout << "v.push_back()" << std::endl;
+		int in = 1;
+		while (in > 0) {
+			std::cin >> in;
+			v.push_back((float)in);
+		}
 
-        std::cout << "v.size(): " << v.size() << std::endl;
-        Print(v);
+		std::cout << "v.size(): " << v.size() << std::endl;
+		Print(v);
 
-        std::cout << "\nv[1]: " << v[1] << std::endl;
+		std::cout << "\nv[1]: " << v[1] << std::endl;
 
-        v.clear();
-        std::cout << "\nv.clear()" << std::endl;
-        std::cout << "v.size(): " << v.size() << std::endl;
+		v.empty();
+		std::cout << "\nv.empty()" << std::endl;
+		std::cout << "v.size(): " << v.size() << std::endl;
 
-        std::cout << "\nv.push_back() 10 elements" << std::endl;
-        in = 10;
-        while (in > 0)
-        {
-            in--;
-            v.push_back((float)in);
-        }
-        std::cout << "v.size(): " << v.size() << std::endl;
+		std::cout << "\nv.push_back() 10 elements" << std::endl;
+		in = 10;
+		while (in > 0) {
+			in--;
+			v.push_back((float)in);
+		}
+		std::cout << "v.size(): " << v.size() << std::endl;
 
-        v.resize(30);
-        std::cout << "v.resize() 30" << std::endl;
-        Print(v);
-        std::cout << "v.size(): " << v.size() << std::endl;
+		v.reserve(40);
+		std::cout << "v.reserve(40) new capacity: " << v.capacity() << std::endl;
 
-        v.resize(2);
-        std::cout << "v.resize() 2" << std::endl;
-        Print(v);
-        std::cout << "v.size(): " << v.size() << std::endl;
+		MyVector<float> b(v);
+		std::cout << "b.size(): " << b.size() << std::endl;
+		std::cout << "b.capacity(): " << b.capacity() << std::endl;
+		Print(b);
 
-        v.reserve(40);
-        std::cout << "v.reserve(40) new capacity: " << v.capacity() << std::endl;
-        
-        MyVector<float> b(v);
-        std::cout << "b.size(): " << b.size() << std::endl;
-        std::cout << "b.capacity(): " << b.capacity() << std::endl;
-        Print(b);
+		v.push_back(54);
+		v.push_back(74);
+		v.push_back(54);
+		v.push_back(34);
 
-        v.push_back(54);
-        v.push_back(74);
-        v.push_back(54);
-        v.push_back(34);
+		b = v;
+		std::cout << "b.size(): " << b.size() << std::endl;
+		std::cout << "b.capacity(): " << b.capacity() << std::endl;
+	}
 
-        b = v;
-        std::cout << "b.size(): " << b.size() << std::endl;
-        std::cout << "b.capacity(): " << b.capacity() << std::endl;
-    }
-
-    // memory leak detection
-    _CrtDumpMemoryLeaks();
+	// memory leak detection
+	_CrtDumpMemoryLeaks();
 
 
 
-    return 0;
+	return 0;
 }
